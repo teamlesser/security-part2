5552d81a78c6119eaa5ef7f2bf0d2fa9ed592e2c
Heidi Hokka <heho1602@student.miun.se>
2018-05-08 16:58:29 +0200

    Created config.php.

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+/*******************************************************************************
+ * File: config.php
+ *
+ * Desc: Contains configuration data for connecting to database
+ * and other things that will be served by the Config-class.
+ *
+ * Date: 2018-05-08
+ ******************************************************************************/
+
+/**
+ * Directly returns array containing configurations.
+ * 2018-05-08: The data is not yet actual.
+ */
+return array(
+    "host" => "localhost",
+    "port" => 5432,
+    "dbname" => "dbname",
+    "user" => "username",
+    "password" => "password",
+    "debug" => true
+);
