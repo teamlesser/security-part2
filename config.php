04ff946eee4a0c532807ac12a1ca8e225d4555f3
Heidi Hokka <heho1602@student.miun.se>
2018-05-11 14:57:43 +0200

    Added actual data to config. Database access is still only local.

--- config.php
+++ config.php
@@ -10,13 +10,14 @@
 
 /**
  * Directly returns array containing configurations.
- * 2018-05-08: The data is not yet actual.
+ * 2018-05-11: The data actual. Please make sure that you
+ * have the role group4lab created before using.
  */
 return array(
     "host" => "localhost",
     "port" => 5432,
-    "dbname" => "dbname",
-    "user" => "username",
-    "password" => "password",
+    "dbname" => "securitylab",
+    "user" => "group4lab",
+    "password" => "s`yWSqL[Zh6@6u[G",
     "debug" => true
 );
