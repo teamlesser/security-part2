412579b60a29a26ae3bbef3cb971d293255b56da
Heidi Hokka <heho1602@student.miun.se>
2018-05-18 16:38:52 +0200

    Added linebreaks in intro paragraphc

--- index.php
+++ index.php
@@ -46,14 +46,18 @@ $title = "Security Lab - Group Four | Home page";
         <a href="user/resetpassword.php">Forgot password?</a>
     </div>
     <div id="return-message"></div>
-    <br><br><br>
+
 
     <section id="main-section">
         <div id="intro">
+
             <h3>Welcome the the Twitter clone of Andreas, Daniel, Heidi, Bernard &amp; Robin</h3>
-            <p>This is a small project for our course Software Security. The aim of this small project is to make a site
-                as secured as possible. By security, we mean a site that cannot be compromised easily. </p>
-            <h4>Things you can do</h4>
+
+            <p>This is a small project for our course Software Security.<br>The aim of this small project is to make a site
+                as secured as possible.<br>By security, we mean a site that cannot be compromised easily.</p>
+
+            <h3>Things you can do</h3>
+
             <p>As a <span class="bold">user</span>, you will be able to</p>
             <ul>
                 <li><a href="user/register.php">Register</a> a new account</li>
@@ -61,12 +65,10 @@ $title = "Security Lab - Group Four | Home page";
                 <li>Post a message</li>
                 <li>Up/Down-vote message(s)</li>
             </ul>
-            <p>As a <span class="bold">hacker</span>, your task is to compromise this site in whatever way you can. Good luck with that!
-            </p>
+            <p>As a <span class="bold">hacker</span>, your task is to compromise this site in whatever way you can. Good luck with that!</p>
         </div>
     </section>
 </main>
-
 
 <footer>
     <p><a href="mailto:dawe1103@student.miun.se">Daniel</a> | <a href="mailto:rovi1601@student.miun.se">Robin</a> | <a href="mailto:lobe1602@student.miun.se">Bernard</a> | <a href="mailto:heho1602@student.miun.se">Heidi</a> | <a href="mailto:anli1606@student.miun.se">Andreas</a></p>
